f59e51280a5e9801ee2bc50fd1b05ba32a48d38d
Michael <checomichael2@gmail.com>
2019-02-06 09:32:10 -0500

    fetching data & storing it in state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/NavBar';
+import axios from 'axios';
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	state = {
+		notes: [],
+	};
+	componentDidMount = async () => {
+		await axios
+			.get('http://localhost:8081/api/notes')
+			.then(res => this.setState({ notes: res.data }));
+		console.log(this.state.notes, 'NOTES');
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<NavBar />
+			</div>
+		);
+	}
 }
 
 export default App;
