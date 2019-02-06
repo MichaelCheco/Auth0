c4ef6d58fb83d8befeaca124378e43ccb16b550d
Michael <checomichael2@gmail.com>
2019-02-06 13:34:48 -0500

    update App.js to work with silent auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import Notes from './components/Notes';
 import Callback from './components/Callback';
 import SecuredRoute from './SecuredRoute/SecuredRoute';
 import ProtectedPage from './components/ProtectedPage';
+import auth0Client from './Auth';
 class App extends Component {
 	state = {
 		notes: [],
+		checkingSession: true,
 	};
 	componentDidMount = async () => {
 		await axios
 			.get('http://localhost:8081/api/notes')
 			.then(res => this.setState({ notes: res.data }));
 		console.log(this.state.notes, 'NOTES');
+		if (this.props.location.pathname === '/callback') {
+			this.setState({ checkingSession: false });
+			return;
+		}
+		try {
+			/* app tries a silentAuth and calls forceUpdate
+       so users can see whatever they asked for */
+			await auth0Client.silentAuth();
+			this.forceUpdate();
+		} catch (err) {
+			if (err.error !== 'login_required') console.log(err.error);
+		}
+		this.setState({ checkingSession: false });
 	};
 
 	render() {
@@ -28,10 +43,16 @@ class App extends Component {
 					render={() => <Notes notes={this.state.notes} />}
 				/>
 				<Route exact path="/callback" component={Callback} />
-				<SecuredRoute path="/secured" component={ProtectedPage} />
+				<SecuredRoute
+					path="/secured"
+					component={ProtectedPage}
+					checkingSession={this.state.checkingSession}
+				/>
 			</div>
 		);
 	}
 }
 
-export default App;
+export default withRouter(App);
+/* withRouter allows us to see what route is 
+being called (this.props.location.pathname) */
