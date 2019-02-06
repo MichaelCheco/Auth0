cdd9d1ecab0654d69587d9ecf9d90976a7ed2b9f
Michael <checomichael2@gmail.com>
2019-02-06 11:34:51 -0500

    securing routes

---
+++ frontend/src/SecuredRoute/SecuredRoute.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import auth0Client from '../Auth';
+
+const SecuredRoute = props => {
+  // path helps us configure our Route
+  // Component tells us what to render in case the user is authenticated
+  const { component: Component, path } = props;
+	return (
+    <Route path={path} render{() => {
+      if (!auth0Client.isAuthenticated()) {
+        auth0Client.signIn()
+        return <div></div>
+      }
+      return <Component />
+    }} />
+  )
+};
+
+export default SecuredRoute;
