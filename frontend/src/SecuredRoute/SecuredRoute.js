42858452d0ccf2438831143049676f423d41d49d
Michael <checomichael2@gmail.com>
2019-02-06 13:35:43 -0500

    updated secure route to avoid redirecting authenticated users to Auth0

--- frontend/src/SecuredRoute/SecuredRoute.js
+++ frontend/src/SecuredRoute/SecuredRoute.js
@@ -5,11 +5,13 @@ import auth0Client from '../Auth';
 const SecuredRoute = props => {
 	// path helps us configure our Route
 	// Component tells us what to render in case the user is authenticated
-	const { component: Component, path } = props;
+	// checkingSession is a boolean that will validate the session
+	const { component: Component, path, checkingSession } = props;
 	return (
 		<Route
 			path={path}
 			render={() => {
+				if (checkingSession) return <h3>Validating Session ... 🚀</h3>;
 				if (!auth0Client.isAuthenticated()) {
 					auth0Client.signIn();
 					return <div />;
