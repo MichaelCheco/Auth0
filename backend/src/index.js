57caa9438b7c2846a0ea846319445e71b769ab2c
Michael <checomichael2@gmail.com>
2019-02-06 08:01:01 -0500

    configured knex file

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser'); // body-parser: This is a library tha
 const cors = require('cors'); // cors: This is a library that you will use to configure Express to add headers stating that your API accepts requests coming from other origins. This is also known as Cross-Origin Resource Sharing (CORS).
 const helmet = require('helmet'); // helmet: This is a library that helps to secure Express apps with various HTTP headers.
 const morgan = require('morgan'); // morgan: This is a library that adds some logging capabilities to your Express app.
-
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const db = knex(knexConfig.development);
 const app = express();
 
 // enhance your app security with Helmet
