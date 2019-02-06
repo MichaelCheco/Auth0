937767ed30a1ba6c44dd73f2b1759bfb2f4702f2
Michael <checomichael2@gmail.com>
2019-02-06 07:54:06 -0500

    added/tested a base endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,3 +17,9 @@ app.use(cors());
 
 // log HTTP requests
 app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+	res.send('Hello');
+});
+
+app.listen(8081, () => console.log('listening on port 8081'));
