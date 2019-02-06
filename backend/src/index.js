e25ba58eef230638bef0e01efcc827cfca6355d5
Michael <checomichael2@gmail.com>
2019-02-06 09:07:01 -0500

    added comments

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser'); // body-parser: This is a library tha
 const cors = require('cors'); // cors: This is a library that you will use to configure Express to add headers stating that your API accepts requests coming from other origins. This is also known as Cross-Origin Resource Sharing (CORS).
 const helmet = require('helmet'); // helmet: This is a library that helps to secure Express apps with various HTTP headers.
 const morgan = require('morgan'); // morgan: This is a library that adds some logging capabilities to your Express app.
-const knex = require('knex');
+const knex = require('knex'); // a SQL query builder
 const knexConfig = require('../knexfile');
 const db = knex(knexConfig.development);
 const app = express();
@@ -19,10 +19,7 @@ app.use(cors());
 // log HTTP requests
 app.use(morgan('combined'));
 
-app.get('/', (req, res) => {
-	res.send('Hello');
-});
-
+// Retrieve all notes from database
 app.get('/api/notes', (req, res) => {
 	db('notes')
 		.then(notes => {
@@ -33,6 +30,7 @@ app.get('/api/notes', (req, res) => {
 		});
 });
 
+// Post a new note to the database
 app.post('/api/notes', (req, res) => {
 	const note = req.body;
 	db('notes')
